1dbd201084cb65cc9569e17504ef54ad7cea664d
uyttyu7532 <uyttyu7532@naver.com>
2021-01-17 20:56:32 +0900

    상위폴더

--- app/src/main/java/com/example/photochangerecord/viewmodel/Folder.kt
+++ app/src/main/java/com/example/photochangerecord/viewmodel/Folder.kt
@@ -1,9 +1,8 @@
 package com.example.photochangerecord.viewmodel
 
-import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.parcel.RawValue
 
 
 @Parcelize
-data class Folder( var title: String, var photos: @RawValue ArrayList<Photo>) : Parcelable
+data class Folder( var title: String, var photos: @RawValue ArrayList<Photo>)
