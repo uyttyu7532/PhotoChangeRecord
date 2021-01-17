900cf53ee6301d5028483f441f90e1dce59ced1e
uyttyu7532 <uyttyu7532@naver.com>
2021-01-17 21:32:41 +0900

    Build Success

--- app/src/main/java/com/example/photochangerecord/viewmodel/Folder.kt
+++ app/src/main/java/com/example/photochangerecord/viewmodel/Folder.kt
@@ -1,8 +1,9 @@
 package com.example.photochangerecord.viewmodel
 
+import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.parcel.RawValue
 
 
 @Parcelize
-data class Folder( var title: String, var photos: @RawValue ArrayList<Photo>)
+data class Folder( var title: String, var photos: @RawValue ArrayList<Photo>) : Parcelable
